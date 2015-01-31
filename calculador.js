8447e2ab16cae203aaecdc5425b91329b28bf3c8
Víctor Jesús Temprano Hernández <victorjesustempranohernandez@MacBook-Pro-de-DJPRANO.local>
2015-01-31 19:32:50 +0100

    Ejercicio 5.5.6 terminado

--- calculador.js
+++ calculador.js
@@ -2,16 +2,22 @@
  * @author Víctor Jesús Temprano Hernández
  */
 function changer(id, newValue) {
-				var element = document.getElementById(id);
-				element.innerHTML = newValue;
-			}
+	var element = document.getElementById(id);
+	element.innerHTML = newValue;
+}
 
-			function adder(id, dato) {
-				var operacion = document.getElementById(id);
-				var numeros = operacion.innerHTML.split("+");
-				var resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
-				
-				console.log(numeros[0]);
-				changer(dato, "= "+resultado);
+function sumar(op, res) {
+	var operacion = document.getElementById(op);
+	var numeros = operacion.innerHTML.split("+");
+	var resultado = parseInt(numeros[0]) + parseInt(numeros[1]);
 
-			}
+	console.log(numeros[0]);
+	changer(res, "= " + resultado);
+
+}
+
+function generar_suma_aleatoria(op) {
+	var n1 = Math.floor(Math.random() * 100) + 1;
+	var n2 = Math.floor(Math.random() * 100) + 1;
+	changer(op,String(n1)+"+"+String(n2));
+}
